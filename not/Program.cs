220cc32a3eea0682670d53a020b465f6a7de456e
Sema Nur AKDAĞ <akdagsemanur3434@gmail.com>
2023-04-26 23:13:51 +0300

    did it

---
+++ not/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+
+küçükse: <
+
+küçük veya eşitse: <=
+
+büyükse: >
+
+büyük veya eşitse: >=
+
+eşitse: ==
+
+eşit değilse: !=
+
+Ve: &&
+
+ya da: || 
+
+*/
+
+/*    Operatör Komutlarının Kullanımı
+
+if (1 == 2 || 1 != 2)
+{
+    // Kod her zaman çalışır
+}
+
+if (1 == 2 && 1 != 2)
+{
+    // Kod hiçbir zaman çalışmaz
+}
+
+if (10 >= 10)
+{
+    // Kod her zaman çalışır
+}
+
+if (10 > 10)
+{
+    // Kod hiçbir zaman çalışmaz
+}
+
+*/
+
+/*  Değişkenler Karşılaştırılabilir
+
+public bool isDead = false;
+
+if (isDead == true)
+{
+   //isDead değişkeni true
+  //olunca çalışır       
+}
+
+
+
+public int health = 1;
+
+if (health <= 0)
+{
+	//health sıfırın altına düşünce
+	//veya eşit olunca çalışır
+}
+
+*/
+
+
+/*
+ 
+
+
+
+   !!!!!  
+          İKİ OBJEDEN BİRİNDE RİGİDBODY OLMAZSA ÇARPIŞMA İŞLEMİ GERÇEKLEŞMEZ
+              İÇİNDEN DİREKT GEÇER
+                              
+                              !!!!!
+ */
+
+
+/*
+ *   "Time.timeSinceLevelLoad"   <<< çok levellı oyunlarda her yeni levela geçtiğimizde timeın baştan başlaması için kullanıyoruz. tek levellı oyun yaptıysak  "Time.time"  bize yeterli olacaktır. 
+ */
